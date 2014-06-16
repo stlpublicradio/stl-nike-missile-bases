2a10b57e4b75a1cfd92e9f563ffee00fdf85d0a9
brentajones <bjones@bjones.net>
2014-06-16 11:13:58 -0500

    initial commit

---
+++ nike.js
@@ -0,0 +1,117 @@
+data={
+    "type": "FeatureCollection",
+    "features": [
+        {
+            "type": "Feature",
+            "properties": {
+                "site": "Control Site",
+                "field_2": "Marine, Ill."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -89.799444,
+                    38.795556
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "site": "Launch Site",
+                "field_2": "Marine, Ill."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -89.790278,
+                    38.832222
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "site": "Control Site",
+                "field_2": "Hecker, Ill."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -89.9475,
+                    38.289722
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "site": "Launch Site",
+                "field_2": "Hecker, Ill."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -89.95,
+                    38.269722
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "site": "Control Site",
+                "field_2": "Pacific, Mo."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -90.754444,
+                    38.408333
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "site": "Launch Site",
+                "field_2": "Pacific, Mo."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -90.755,
+                    38.403611
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "site": "Control Site",
+                "field_2": "Alton, Ill."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -90.508889,
+                    38.981944
+                ]
+            }
+        },
+        {
+            "type": "Feature",
+            "properties": {
+                "site": "Launch Site",
+                "field_2": "Alton, Ill."
+            },
+            "geometry": {
+                "type": "Point",
+                "coordinates": [
+                    -90.51,
+                    38.995278
+                ]
+            }
+        }
+    ]
+}
